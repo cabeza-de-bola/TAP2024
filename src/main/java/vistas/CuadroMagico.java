68e12c14c4dafd0da80401d910f82fd3b0f493af
Ian Alexander Amezcua <21031599@itcelaya.edu.mx>
2024-02-06 18:27:39 -0600

    Screen Cuadro Magico

---
+++ src/main/java/vistas/CuadroMagico.java
@@ -0,0 +1,19 @@
+package vistas;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class CuadroMagico extends Stage {
+    private Scene escena;
+    public CuadroMagico(){
+        this.setTitle("Cuadro Mágico");
+        this.setScene();
+        this.show();
+    }
+
+
+    private void CrearUI(){
+        escena = new Scene(new Button("Da click"));
+    }
+}
